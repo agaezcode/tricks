aa7618fbcee3131507eb2758d2d5df388a27efaf
agaezcode <agaezcode@gmail.com>
2015-02-08 18:08:48 +0000

    Create jquery-edit-attr.js

---
+++ jquery-edit-attr.js
@@ -0,0 +1,10 @@
+// Jquery method to replace attributes if it's empty or null
+$('img').each(function(){
+ var alt = this.getAttribute('alt');
+ if (alt === null) {
+   alert('null, the "alt" atribute doesn't exists')
+  } else if (alt === '') {
+   alert('empty') // or
+   // $("img").attr("src","default-picture.png");
+  }
+});
